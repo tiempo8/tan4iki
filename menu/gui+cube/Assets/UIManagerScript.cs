7acc5283b877b153fcd7cb753d154dcb2d253d54
NIIKOB <nikitabel0403@gmail.com>
2015-10-08 21:12:27 +0300

    check

--- menu/gui+cube/Assets/UIManagerScript.cs
+++ menu/gui+cube/Assets/UIManagerScript.cs
@@ -6,4 +6,43 @@ public class UIManagerScript : MonoBehaviour {
 	public void StartGame() {
 		Application.LoadLevel("main");
 	}
+	public Animator startButton;
+	
+	public Animator settingsButton;
+
+	public Animator quitButton;
+
+	public Animator dialog;
+
+	public void OpenSettings()
+		
+	{
+		
+		startButton.SetBool("isHidden", true);
+		
+		settingsButton.SetBool("isHidden", true);
+
+		quitButton.SetBool("isHidden", true);
+		dialog.enabled = true;
+		
+		dialog.SetBool("isHidden", false);
+
+		
+	}
+
+
+	public void CloseSettings()
+		
+	{
+		
+		startButton.SetBool("isHidden", false);
+		
+		settingsButton.SetBool("isHidden", false);
+
+		quitButton.SetBool("isHidden", false);
+		
+		dialog.SetBool("isHidden", true);
+		
+	}
+
 }
